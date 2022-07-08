d74640cc576740bf736c66dbef7460ed573e29f6
hexyminx <hexyminx@gmail.com>
2022-07-08 06:35:18 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const characters = {
+    'izuku midoriya':{
+        'age': 14,
+        'hero name': 'Deku',
+        'quirk': 'One For All',
+    },   
+    'katsuki bakugo':{
+        'age': 14,
+        'hero name': 'Dynamight',
+        'quirk': 'Explosion',
+    },
+    'unknown':{
+        'age': 'unknown',
+        'hero name': 'unknown',
+        'quirk': 'unknown',
+    } 
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:characterName', (request, response) => {
+    const characterName = request.params.characterName.toLowerCase()
+    if(characters[characterName]){
+        response.json(characters[characterName])
+    }else {
+        response.json('unknown')
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is running on port ${PORT}! You better go catch it!`)
+})
